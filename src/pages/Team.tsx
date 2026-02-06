b94811b91e57a7b96f7f91ab5dd358ac5ce529f8
Archil Gelashvili <ar.gelashvili@gmail.com>
2026-02-06 13:40:07 +0400

    feature/add team members page content

--- src/pages/Team.tsx
+++ src/pages/Team.tsx
@@ -1,5 +1,31 @@
+import MainTeamMembersCard from "../components/main/MainTeamMembersCard";
+import MainTitle from "../components/main/MainTitle";
+import { TEAM_MEMBERS } from "../constants/teamMembers";
+
 const Team = () => {
-  return <div>Team</div>;
+  return (
+    <div className="max-w-7xl mx-auto flex flex-col gap-8">
+      <MainTitle
+        department={"Housekeeping Department"}
+        page={"გუნდი"}
+        title={"ჩემი გუნდი"}
+        description={"თქვენი დეპარტამენტის თანამშრომლები"}
+      />
+      <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-6">
+        {TEAM_MEMBERS.map((tm) => (
+          <MainTeamMembersCard
+            key={tm.id}
+            avatar={tm.avatar}
+            name={tm.name}
+            lastName={tm.lastName}
+            isOnShift={tm.isOnShift}
+            shifts={tm.shifts}
+            position={tm.position}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Team;
