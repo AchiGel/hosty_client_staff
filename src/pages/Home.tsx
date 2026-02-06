af2407e2cf1b6392010ca4e44b1f605d159319aa
Archil Gelashvili <ar.gelashvili@gmail.com>
2026-02-06 13:08:45 +0400

    feature/add requests blur

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import MainDashboardBlur from "../components/main/MainDashboardBlur";
 import MainFilterButton from "../components/main/MainFilterButton";
 import MainRequestCard from "../components/main/MainRequestCard";
 import MainTable from "../components/main/MainTable";
@@ -16,31 +17,34 @@ const Home = () => {
         title={"My Department"}
         description={"Only housekeeping department requests"}
       />
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
-        {DASHBOARD_REQUESTS.map((r) => (
-          <MainRequestCard
-            key={r.id}
-            icon={r.icon}
-            title={r.title}
-            quantity={r.quantity}
-            id={r.id}
-          />
-        ))}
+      <div className="relative">
+        <MainDashboardBlur />
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+          {DASHBOARD_REQUESTS.map((r) => (
+            <MainRequestCard
+              key={r.id}
+              icon={r.icon}
+              title={r.title}
+              quantity={r.quantity}
+              id={r.id}
+            />
+          ))}
+        </div>
+        <div className="flex flex-wrap items-center gap-3 py-2 overflow-x-auto">
+          {FILTER_BUTTONS.map((b) => (
+            <MainFilterButton
+              key={b.id}
+              title={b.title}
+              label={b.label}
+              icon={b.icon}
+            />
+          ))}
+        </div>
+        <MainTable
+          tableData={DASHBOARD_TABLE_HEADER}
+          tableRowData={DASHBOARD_TABLE_REQUEST_ROW}
+        />
       </div>
-      <div className="flex flex-wrap items-center gap-3 py-2 overflow-x-auto">
-        {FILTER_BUTTONS.map((b) => (
-          <MainFilterButton
-            key={b.id}
-            title={b.title}
-            label={b.label}
-            icon={b.icon}
-          />
-        ))}
-      </div>
-      <MainTable
-        tableData={DASHBOARD_TABLE_HEADER}
-        tableRowData={DASHBOARD_TABLE_REQUEST_ROW}
-      />
     </div>
   );
 };
